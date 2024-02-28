3a1c9955433ffa55d0d24db380fcc745d038a9f8
fengjianhao <lingj.jhf@outlook.com>
2024-02-28 18:34:24 +0800

    StandingsRow

--- lib/src/features/data/presentation/standings.dart
+++ lib/src/features/data/presentation/standings.dart
@@ -1,6 +1,4 @@
-
-class StandingsRow  {
-
+class StandingsRow {
   const StandingsRow({
     required this.team,
     this.played = 0,
@@ -9,15 +7,18 @@ class StandingsRow  {
     this.points = 0,
     this.position = 0,
   });
-  
+
   //队名
   final String team;
-  
+
   //场次
   final int played;
-  
+
   //胜
   final int won;
+
+  //平
+  final int drawn;
 
   //负
   final int lost;
@@ -27,4 +28,10 @@ class StandingsRow  {
 
   //排名
   final int position;
+
+  //进球
+  final int goalsFor;  
+
+  //失球
+  final int goalsAgainst;
 }
